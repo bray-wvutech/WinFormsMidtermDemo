deae0afe7aa49c771af343207ea3193450cfb834
Brian Ray <bray_stenovations@yahoo.com>
2024-10-17 10:39:09 -0400

    Add project files.

---
+++ WinFormsMidtermDemo/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WinFormsMidtermDemo;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        structAssignmentButton = new Button();
+        paramTest1Button = new Button();
+        paramTest2Button = new Button();
+        paramTest3Button = new Button();
+        paramTest4Button = new Button();
+        paramTest5Button = new Button();
+        dateTimeButton = new Button();
+        cardsImageList = new ImageList(components);
+        cardPictureBox = new PictureBox();
+        mainOpenFileDialog = new OpenFileDialog();
+        openButton = new Button();
+        mainSaveFileDialog = new SaveFileDialog();
+        personListBox = new ListBox();
+        phoneLabel = new Label();
+        phoneButton = new Button();
+        ((System.ComponentModel.ISupportInitialize)cardPictureBox).BeginInit();
+        SuspendLayout();
+        // 
+        // structAssignmentButton
+        // 
+        structAssignmentButton.Location = new Point(131, 271);
+        structAssignmentButton.Name = "structAssignmentButton";
+        structAssignmentButton.Size = new Size(169, 29);
+        structAssignmentButton.TabIndex = 0;
+        structAssignmentButton.Text = "Struct Assignment";
+        structAssignmentButton.UseVisualStyleBackColor = true;
+        structAssignmentButton.Click += structAssignmentButton_Click;
+        // 
+        // paramTest1Button
+        // 
+        paramTest1Button.Location = new Point(131, 14);
+        paramTest1Button.Name = "paramTest1Button";
+        paramTest1Button.Size = new Size(173, 29);
+        paramTest1Button.TabIndex = 1;
+        paramTest1Button.Text = "Param Test 1";
+        paramTest1Button.UseVisualStyleBackColor = true;
+        paramTest1Button.Click += paramTest1Button_Click;
+        // 
+        // paramTest2Button
+        // 
+        paramTest2Button.Location = new Point(131, 63);
+        paramTest2Button.Name = "paramTest2Button";
+        paramTest2Button.Size = new Size(173, 29);
+        paramTest2Button.TabIndex = 2;
+        paramTest2Button.Text = "Param Test 2";
+        paramTest2Button.UseVisualStyleBackColor = true;
+        paramTest2Button.Click += paramTest2Button_Click;
+        // 
+        // paramTest3Button
+        // 
+        paramTest3Button.Location = new Point(131, 111);
+        paramTest3Button.Name = "paramTest3Button";
+        paramTest3Button.Size = new Size(173, 29);
+        paramTest3Button.TabIndex = 3;
+        paramTest3Button.Text = "Param Test 3";
+        paramTest3Button.UseVisualStyleBackColor = true;
+        paramTest3Button.Click += paramTest3Button_Click;
+        // 
+        // paramTest4Button
+        // 
+        paramTest4Button.Location = new Point(131, 162);
+        paramTest4Button.Name = "paramTest4Button";
+        paramTest4Button.Size = new Size(173, 29);
+        paramTest4Button.TabIndex = 5;
+        paramTest4Button.Text = "Param Test 4";
+        paramTest4Button.UseVisualStyleBackColor = true;
+        paramTest4Button.Click += paramTest4Button_Click;
+        // 
+        // paramTest5Button
+        // 
+        paramTest5Button.Location = new Point(131, 216);
+        paramTest5Button.Name = "paramTest5Button";
+        paramTest5Button.Size = new Size(173, 29);
+        paramTest5Button.TabIndex = 6;
+        paramTest5Button.Text = "Array Test";
+        paramTest5Button.UseVisualStyleBackColor = true;
+        paramTest5Button.Click += paramTest5Button_Click;
+        // 
+        // dateTimeButton
+        // 
+        dateTimeButton.Location = new Point(131, 324);
+        dateTimeButton.Name = "dateTimeButton";
+        dateTimeButton.Size = new Size(169, 29);
+        dateTimeButton.TabIndex = 7;
+        dateTimeButton.Text = "DateTime";
+        dateTimeButton.UseVisualStyleBackColor = true;
+        dateTimeButton.Click += dateTimeButton_Click;
+        // 
+        // cardsImageList
+        // 
+        cardsImageList.ColorDepth = ColorDepth.Depth32Bit;
+        cardsImageList.ImageStream = (ImageListStreamer)resources.GetObject("cardsImageList.ImageStream");
+        cardsImageList.TransparentColor = System.Drawing.Color.Transparent;
+        cardsImageList.Images.SetKeyName(0, "p0.png");
+        cardsImageList.Images.SetKeyName(1, "p1.png");
+        cardsImageList.Images.SetKeyName(2, "p2.png");
+        cardsImageList.Images.SetKeyName(3, "p3.png");
+        cardsImageList.Images.SetKeyName(4, "p4.png");
+        cardsImageList.Images.SetKeyName(5, "p5.png");
+        cardsImageList.Images.SetKeyName(6, "p6.png");
+        cardsImageList.Images.SetKeyName(7, "p7.png");
+        cardsImageList.Images.SetKeyName(8, "p8.png");
+        cardsImageList.Images.SetKeyName(9, "p9.png");
+        cardsImageList.Images.SetKeyName(10, "p10.png");
+        cardsImageList.Images.SetKeyName(11, "p11.png");
+        cardsImageList.Images.SetKeyName(12, "p12.png");
+        cardsImageList.Images.SetKeyName(13, "p13.png");
+        cardsImageList.Images.SetKeyName(14, "p14.png");
+        cardsImageList.Images.SetKeyName(15, "p15.png");
+        cardsImageList.Images.SetKeyName(16, "p16.png");
+        cardsImageList.Images.SetKeyName(17, "p17.png");
+        cardsImageList.Images.SetKeyName(18, "p18.png");
+        cardsImageList.Images.SetKeyName(19, "p19.png");
+        cardsImageList.Images.SetKeyName(20, "p20.png");
+        cardsImageList.Images.SetKeyName(21, "p21.png");
+        cardsImageList.Images.SetKeyName(22, "p22.png");
+        cardsImageList.Images.SetKeyName(23, "p23.png");
+        cardsImageList.Images.SetKeyName(24, "p24.png");
+        cardsImageList.Images.SetKeyName(25, "p25.png");
+        cardsImageList.Images.SetKeyName(26, "p26.png");
+        cardsImageList.Images.SetKeyName(27, "p27.png");
+        cardsImageList.Images.SetKeyName(28, "p28.png");
+        cardsImageList.Images.SetKeyName(29, "p29.png");
+        cardsImageList.Images.SetKeyName(30, "p30.png");
+        cardsImageList.Images.SetKeyName(31, "p31.png");
+        cardsImageList.Images.SetKeyName(32, "p32.png");
+        cardsImageList.Images.SetKeyName(33, "p33.png");
+        cardsImageList.Images.SetKeyName(34, "p34.png");
+        cardsImageList.Images.SetKeyName(35, "p35.png");
+        cardsImageList.Images.SetKeyName(36, "p36.png");
+        cardsImageList.Images.SetKeyName(37, "p37.png");
+        cardsImageList.Images.SetKeyName(38, "p38.png");
+        cardsImageList.Images.SetKeyName(39, "p39.png");
+        cardsImageList.Images.SetKeyName(40, "p40.png");
+        cardsImageList.Images.SetKeyName(41, "p41.png");
+        cardsImageList.Images.SetKeyName(42, "p42.png");
+        cardsImageList.Images.SetKeyName(43, "p43.png");
+        cardsImageList.Images.SetKeyName(44, "p44.png");
+        cardsImageList.Images.SetKeyName(45, "p45.png");
+        cardsImageList.Images.SetKeyName(46, "p46.png");
+        cardsImageList.Images.SetKeyName(47, "p47.png");
+        cardsImageList.Images.SetKeyName(48, "p48.png");
+        cardsImageList.Images.SetKeyName(49, "p49.png");
+        cardsImageList.Images.SetKeyName(50, "p50.png");
+        cardsImageList.Images.SetKeyName(51, "p51.png");
+        // 
+        // cardPictureBox
+        // 
+        cardPictureBox.Location = new Point(537, 46);
+        cardPictureBox.Name = "cardPictureBox";
+        cardPictureBox.Size = new Size(279, 295);
+        cardPictureBox.TabIndex = 8;
+        cardPictureBox.TabStop = false;
+        // 
+        // mainOpenFileDialog
+        // 
+        mainOpenFileDialog.FileName = "openFileDialog1";
+        // 
+        // openButton
+        // 
+        openButton.Location = new Point(369, 102);
+        openButton.Name = "openButton";
+        openButton.Size = new Size(94, 29);
+        openButton.TabIndex = 9;
+        openButton.Text = "Open File";
+        openButton.UseVisualStyleBackColor = true;
+        openButton.Click += openButton_Click;
+        // 
+        // personListBox
+        // 
+        personListBox.FormattingEnabled = true;
+        personListBox.Location = new Point(893, 46);
+        personListBox.Name = "personListBox";
+        personListBox.Size = new Size(150, 244);
+        personListBox.TabIndex = 10;
+        personListBox.SelectedIndexChanged += personListBox_SelectedIndexChanged;
+        // 
+        // phoneLabel
+        // 
+        phoneLabel.BorderStyle = BorderStyle.FixedSingle;
+        phoneLabel.Location = new Point(1074, 159);
+        phoneLabel.Name = "phoneLabel";
+        phoneLabel.Size = new Size(149, 25);
+        phoneLabel.TabIndex = 11;
+        phoneLabel.Text = "label1";
+        // 
+        // phoneButton
+        // 
+        phoneButton.Location = new Point(893, 312);
+        phoneButton.Name = "phoneButton";
+        phoneButton.Size = new Size(150, 29);
+        phoneButton.TabIndex = 12;
+        phoneButton.Text = "Load Phone Book";
+        phoneButton.UseVisualStyleBackColor = true;
+        phoneButton.Click += phoneButton_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1260, 409);
+        Controls.Add(phoneButton);
+        Controls.Add(phoneLabel);
+        Controls.Add(personListBox);
+        Controls.Add(openButton);
+        Controls.Add(cardPictureBox);
+        Controls.Add(dateTimeButton);
+        Controls.Add(paramTest5Button);
+        Controls.Add(paramTest4Button);
+        Controls.Add(paramTest3Button);
+        Controls.Add(paramTest2Button);
+        Controls.Add(paramTest1Button);
+        Controls.Add(structAssignmentButton);
+        Name = "Form1";
+        Text = "Form1";
+        Load += Form1_Load;
+        ((System.ComponentModel.ISupportInitialize)cardPictureBox).EndInit();
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Button structAssignmentButton;
+    private Button paramTest1Button;
+    private Button paramTest2Button;
+    private Button paramTest3Button;
+    private Button paramTest4Button;
+    private Button paramTest5Button;
+    private Button dateTimeButton;
+    private ImageList cardsImageList;
+    private PictureBox cardPictureBox;
+    private OpenFileDialog mainOpenFileDialog;
+    private Button openButton;
+    private SaveFileDialog mainSaveFileDialog;
+    private ListBox personListBox;
+    private Label phoneLabel;
+    private Button phoneButton;
+}
---
+++ WinFormsMidtermDemo/Form1.cs
@@ -0,0 +1,303 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
+
+namespace WinFormsMidtermDemo;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        cardPictureBox.Image = cardsImageList.Images[32];
+    }
+
+    private void ParamValueTester(int number)
+
+    {
+        number = 3;
+    }
+
+    private void Test()
+    {
+        int x = 1;
+        ParamValueTester(x);
+        int y = x;
+        MessageBox.Show(y.ToString());
+    }
+
+    private void ParamValueTester2(out int number)
+    {
+        number = 3;
+    }
+
+    private void Test2()
+    {
+        int x = 1;
+        ParamValueTester2(out x);
+        int y = x;
+        MessageBox.Show(y.ToString());
+    }
+
+    private void ParamValueTester3(int[] numbers)
+    {
+        numbers[0] = 27;
+    }
+
+    private void Test3()
+    {
+        int[] numArray = { 1, 2, 3 };
+        ParamValueTester3(numArray);
+        int y = numArray[0];
+        MessageBox.Show(y.ToString());
+    }
+
+    private void ParamValueTester4(List<int> numbers)
+    {
+        numbers = null;
+    }
+
+    private void Test4()
+    {
+        List<int> numList = new List<int>() { 1, 2, 3 };
+        ParamValueTester4(numList);
+        int y = 0;
+        if (numList != null)
+        {
+            y = numList[0];
+        }
+        MessageBox.Show(y.ToString());
+    }
+
+    private void Test5()
+    {
+        int[] array1 = new int[3] { 1, 2, 3 };
+        int[] array2 = array1;
+        array1[0] = 32;
+        int y = array2[0];
+        MessageBox.Show(y.ToString());
+    }
+
+    private void paramTest1Button_Click(object sender, EventArgs e)
+    {
+        Test();
+    }
+
+    private void paramTest2Button_Click(object sender, EventArgs e)
+    {
+        Test2();
+    }
+
+    private void paramTest3Button_Click(object sender, EventArgs e)
+    {
+        Test3();
+    }
+
+    private void paramTest4Button_Click(object sender, EventArgs e)
+    {
+        Test4();
+    }
+
+    private void paramTest5Button_Click(object sender, EventArgs e)
+    {
+        Test5();
+    }
+
+    private bool IsEqualNoCase(string? str1, string? str2)
+    {
+        if (str1.ToUpper() == str2.ToUpper())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool IsEqualNoCase2(string? str1, string? str2)
+    {
+        return string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private bool IsEqualNoCase3(string? str1, string? str2)
+    {
+        // maybe you want this maybe you don't
+        if ((str1 == null) && (str2 == null))
+        {
+            return true;
+        }
+
+        if ((str1 == null) || (str2 == null))
+        {
+            return false;
+        }
+
+        if (str1.ToUpper() == str2.ToUpper())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private enum Color { Red, Green, Blue }
+    private string ColorToString(Color color)
+    {
+        return color.ToString();
+    }
+
+    private string ColorToString2(Color color)
+    {
+        switch (color)
+        {
+            case Color.Red:
+                return "Red";
+            case Color.Blue:
+                return "Blue";
+            case Color.Green:
+                return "Green";
+            default:
+                return "";
+        }
+    }
+
+    // STRUCT TESTS
+    struct PersonStruct
+    {
+        public int age;
+    }
+
+    private void StructTest()
+    {
+        PersonStruct p1 = new PersonStruct();
+        p1.age = 32;
+
+        // even though they use new structs are VALUE types NOT reference types
+        PersonStruct p2 = p1;
+        p1.age = 75;
+        MessageBox.Show(p2.age.ToString());
+
+        // you can't directly compare structs
+        // you will get an error
+        //if (p1 == p2)
+        //{
+        //    MessageBox.Show("can't compare structs");
+        //}
+
+        StructParamTest(p2);
+        MessageBox.Show(p2.age.ToString());
+    }
+
+    private void StructParamTest(PersonStruct p)
+    {
+        p.age = 99;
+    }
+
+    private void structAssignmentButton_Click(object sender, EventArgs e)
+    {
+        StructTest();
+    }
+
+    // DATE TIME TESTS
+    private void DateTimeTest()
+    {
+        DateTime dt1 = DateTime.Now;
+        DateTime dt2;
+        dt2 = dt1;
+
+        int[] a1 = new int[3];
+        int[] a2 = a1;
+
+        string dt1ptr = GetPtr(dt1);
+        string dt2ptr = GetPtr(dt2);
+        string a1ptr = GetPtr(a1);
+        string a2ptr = GetPtr(a2);
+
+        string msg = "";
+        msg += $"dt1: {dt1.ToString()}\n";
+        msg += $"dt2: {dt2.ToString()}\n";
+        msg += $"dt1 ptr: {dt1ptr}\n";
+        msg += $"dt2 ptr: {dt2ptr}\n";
+        msg += $"a1 ptr: {a1ptr}\n";
+        msg += $"a2 ptr: {a2ptr}\n";
+        MessageBox.Show(msg);
+    }
+
+    public static string GetPtr(object a)
+    {
+        var ptr = Unsafe.As<object, IntPtr>(ref a);
+        return $"0x{ptr:X}";
+    }
+
+    private void dateTimeButton_Click(object sender, EventArgs e)
+    {
+        DateTimeTest();
+    }
+
+    // OTHER STUFF
+    private const string FILE_PATH = @"C:\Users\bray_\source\repos\2024 fall c sharp class\WinFormsMidtermDemo\WinFormsMidtermDemo\hands\";
+    private void openButton_Click(object sender, EventArgs e)
+    {
+        mainSaveFileDialog.InitialDirectory = FILE_PATH;
+        mainSaveFileDialog.DefaultExt = "txt";
+        if (mainSaveFileDialog.ShowDialog() == DialogResult.OK)
+        {
+
+        }
+    }
+
+    private struct PhoneBookEntry
+    {
+        public string name;
+        public string phone;
+    }
+
+    private const string PHONE_BOOK_PATH = @"C:\Users\bray_\source\repos\2024 fall c sharp class\WinFormsMidtermDemo\WinFormsMidtermDemo\hands\TextFile1.txt";
+    private List<PhoneBookEntry> entries = new List<PhoneBookEntry>();
+
+    private void LoadPhoneBook()
+    {
+        try
+        {
+            using (var reader = new StreamReader(PHONE_BOOK_PATH))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        string[] tokens = line.Split(",");
+                        PhoneBookEntry entry = new();
+                        entry.name = tokens[0];
+                        entry.phone = tokens[1];
+                        entries.Add(entry);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void EntriesToList()
+    {
+        personListBox.Items.Clear();
+        foreach (PhoneBookEntry entry in entries)
+        {
+            personListBox.Items.Add(entry.name);
+        }
+    }
+
+    private void phoneButton_Click(object sender, EventArgs e)
+    {
+        LoadPhoneBook();
+        EntriesToList();
+    }
+
+    private void personListBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        phoneLabel.Text = entries[personListBox.SelectedIndex].phone;
+    }
+}
